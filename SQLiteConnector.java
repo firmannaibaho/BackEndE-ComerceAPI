0e911ba58928986ee8e4d507c29749b1577e9512
firmannaibaho <115171322+firmannaibaho@users.noreply.github.com>
2023-05-30 14:09:43 +0800

    Update SQLiteConnector.java

--- SQLiteConnector.java
+++ SQLiteConnector.java
@@ -1,3 +1,5 @@
+package org.example;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -6,6 +8,14 @@ public class SQLiteConnector {
     private static final String DB_URL = "jdbc:sqlite:C:/sqlite/sqlitedb_tugas2";
 
     public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            System.err.println("SQLite JDBC driver not found.");
+            e.printStackTrace();
+        }
         return DriverManager.getConnection(DB_URL);
     }
 }
+
+
